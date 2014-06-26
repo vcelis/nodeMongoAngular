031bd41d5c8ff62d7d95a98435ca5b6a20ee0789
vcelis <vincent.celis@gmail.com>
2014-06-26 19:19:09 +0200

    http services...

--- lesson5/httpService/static/js/main.js
+++ lesson5/httpService/static/js/main.js
@@ -1,1 +1,38 @@
-main.js
+angular.module('myApp', [])
+  .controller('myController', ['$scope', '$http', function($scope, $http) {
+    $scope.words = [];
+    $scope.status = '';
+    $scope.newWord = '';
+
+    $scope.getWords = function() {
+      $http.get('http://localhost:3333/words')
+        .success(function(data, status, headers, config) {
+          $scope.words = data;
+        })
+        .error(function(data, status, headers, config) {
+          $scope.status = data;
+        });
+    };
+
+    $scope.addWord = function() {
+      $http.post('http://localhost:3333/add', {word: $scope.newWord})
+        .success(function(data, status, headers, config) {
+          $scope.newWord = '';
+          $scope.status = data;
+          $scope.getWords();
+        })
+        .error(function(data, status, headers, config) {
+          $scope.status = data;
+        });
+    };
+
+    $scope.removeWord = function(deleteWord) {
+      $http.post('http://localhost:3333/remove', {word: deleteWord})
+        .success(function(data, status, headers, config) {
+
+        })
+        .error(function(data, status, headers, config){
+
+        });
+    };
+  }]);
