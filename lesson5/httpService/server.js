01358e803fc9ce21346539b6544553f7d18ab49f
vcelis <vincent.celis@gmail.com>
2014-06-26 13:06:15 +0200

    http services

---
+++ lesson5/httpService/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use('/', express.static('./static'));
+app.use(bodyParser());
+
+var words = [ 'test' ];
+
+app.get('/words', function(req, res) {
+  res.json(words);
+});
+
+app.post('/add', function(req, res) {
+  var index = words.indexOf(req.body.word);
+  if (index !== -1) {
+    res.json(400, 'Word already exists.');
+  } else {
+    words.push(req.body.word);
+    res.json(200, 'Word added');
+  }
+});
+
+app.post('/remove', function(req, res) {
+  var index = words.indexOf(req.body.word);
+  if (index !== -1) {
+    words.splice(index, 1);
+    res.json(200, 'Word removed');
+  }
+})
+
+app.listen(3333);
