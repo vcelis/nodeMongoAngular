0e9d1862487a0f321145cd717822ac80f1735768
vcelis <vincent.celis@gmail.com>
2014-06-27 13:07:37 +0200

    paging nice...

--- lesson7/controllers/wordsController.js
+++ lesson7/controllers/wordsController.js
@@ -2,17 +2,18 @@ var MongoClient = require('mongodb').MongoClient;
 
 var words = null;
 MongoClient.connect('mongodb://localhost/', function(err, db) {
-  words = db.db('words').collection('word_stats');
+  words = db.db('words').collection('words');
 });
 
 exports.getWords = function(req, res) {
   if (words) {
-    words.find({ }, {limit: 5}, function(err, cursor) {
+    var options = {limit: req.query.limit, skip: req.query.skip};
+    words.find({ }, options, function(err, cursor) {
       cursor.toArray(function(err, wordsArr) {
         res.json(wordsArr);
       });
     });
   } else {
-    res.json(500, {});
+    res.json(503, {});
   }
 };
--- lesson7/static/js/main.js
+++ lesson7/static/js/main.js
@@ -1,18 +1,44 @@
 angular.module('myApp', [])
   .controller('tableController', ['$scope', '$http', function($scope, $http) {
     $scope.words = [];
+    $scope.limit = 5;
+    $scope.skip = 0;
+    $scope.skipEnd = 0;
 
     $scope.getWords = function() {
-      $http({url: 'http://localhost/words', method: 'GET', params: {}})
+      var params = {limit: $scope.limit, skip: $scope.skip};
+      $http({url: 'http://localhost:3333/words', method: 'GET', params: params})
         .success(function(data, status, headers, config) {
           $scope.words = data;
+          $scope.skipEnd = $scope.skip + $scope.words.length;
         })
         .error(function(data, status, headers, config) {
           $scope.words = [];
+          $scope.skipEnd = $scope.skip + $scope.words.length;
         });
     };
 
     $scope.find = function() {
+      // init skip
+      $scope.skip = 0;
       $scope.getWords();
+    };
+
+    $scope.next = function() {
+      if ($scope.words.length === $scope.limit) {
+        $scope.skip += parseInt($scope.limit);
+        $scope.getWords();
+      }
+    };
+
+    $scope.prev = function() {
+      if ($scope.skip > 0) {
+        if ($scope.skip >= parseInt($scope.limit)) {
+          $scope.skip -= parseInt($scope.limit);
+        } else {
+          $scope.skip = 0;
+        }
+        $scope.getWords();
+      }
     };
   }]);
